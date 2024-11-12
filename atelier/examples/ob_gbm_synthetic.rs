d13d8b47ef56d29b8a74fae0533b8b05fc1bf44b
IFFranciscoME <if.francisco.me@gmail.com>
2024-11-12 03:22:36 -0600

    Refactoring of examples

---
+++ atelier/examples/ob_gbm_synthetic.rs
@@ -0,0 +1,100 @@
+use atelier::data::market::Orderbook;
+use atelier::generators::brownian;
+use atelier::metrics::market;
+use atelier::metrics::market::MarketMetric;
+
+fn main() {
+
+    // Parameters for the first Order Book
+    let bid_price = 50_000.00;
+    let ask_price = 50_100.00;
+    let tick_size = 100.0;
+    let n_levels = 200;
+    let n_orders = 10;
+
+    // Orderbook data structure 
+    let orderbook = Orderbook::synthetize(bid_price, ask_price, tick_size, n_levels, n_orders);
+    let mut n_orderbooks: Vec<Orderbook> = vec![];
+    n_orderbooks.push(orderbook);
+    
+    // Progression model parameters
+    let n_progressions: usize = 50;
+    let mu = 0.001;
+    let sigma = 0.0025;
+
+    // ---------------------------------------------------- Progressions Generation -- //
+    // ---------------------------------------------------- ----------------------- -- //
+    
+    for i in 0..=n_progressions {
+
+        let i_bid_price = n_orderbooks[i].bids[0].price;
+        let i_ask_price = n_orderbooks[i].asks[0].price;
+
+        // Generate a single progression for the price change
+        let i_ret_gbm_bids: f64 = brownian::gbm_return(
+            i_bid_price, mu, sigma, 0.1, 1).unwrap()[0];
+        let i_ret_gbm_asks: f64 = brownian::gbm_return(
+            i_ask_price, mu, sigma, 0.1, 1).unwrap()[0];
+
+        let i_orderbook = Orderbook::synthetize(
+            i_bid_price - i_ret_gbm_bids,
+            i_ask_price + i_ret_gbm_asks,
+            tick_size,
+            n_levels,
+            n_orders,
+        );
+        n_orderbooks.push(i_orderbook);
+    }
+
+    // ------------------------------------------------ Market Metrics Calculations -- //
+    // ------------------------------------------------ --------------------------- -- //
+    
+    let mut v_mids: Vec<f64> = vec![];
+    let mut v_vwap: Vec<f64> = vec![];
+    let mut v_spreads: Vec<f64> = vec![];
+
+    //Compute the Spread
+    for i in 0..=n_progressions {
+        
+        // Compute the spreads
+        v_spreads.push(
+            market::Spread::compute(
+                &n_orderbooks[i].bids[0].price,
+                &n_orderbooks[i].asks[0].price,
+            0)
+        );        
+
+        // Compute the mids
+        v_mids.push(
+            market::Midprice::compute(
+                &n_orderbooks[i].bids[0].price,
+                &n_orderbooks[i].asks[0].price,
+                0)
+        );
+
+        // Compute the Volume-Weighted Average Price
+        let iter_bids: Vec<_> = n_orderbooks[i]
+            .bids
+            .clone()
+            .into_iter()
+            .map(|x| vec![x.price, x.volume])
+            .collect();
+        
+        let iter_asks: Vec<_> = n_orderbooks[i]
+            .asks
+            .clone()
+            .into_iter()
+            .map(|x| vec![x.price, x.volume])
+            .collect();
+        
+        // Compute the VWAP
+        v_vwap.push(market::VWAP::compute(&iter_bids.clone(), &iter_asks.clone(), 4));
+
+    };
+    
+    // Generate output data
+    println!("v_spreads: {:?}", v_spreads);
+    println!("v_mids: {:?}", v_mids);
+    println!("v_vwap: {:?}", v_vwap);
+    
+}
---
+++ atelier/examples/ob_synthetic.rs
@@ -0,0 +1,77 @@
+use atelier::data::market::Orderbook;
+use atelier::generators::hawkes;
+use std::time::{SystemTime, UNIX_EPOCH};
+use atelier::data::market;
+
+fn main() {
+   
+    // Parameters for the Initial State for the Order Book
+    let bid_price = 70_000.00;
+    let ask_price = 70_100.00;
+    let tick_size = 100.0;
+    let n_levels = 4;
+    let n_orders = 2;
+
+    let n_orderbooks = Orderbook::synthetize(
+        bid_price,
+        ask_price,
+        tick_size, 
+        n_levels, 
+        n_orders);
+    
+    // println!("n_orderbooks: {:?}", n_orderbooks);
+
+    // Define the step window (seconds)
+    let step_window: f64 = 1.0;
+    
+    // Within the step window, all the events need to take place 
+    let market_orders_times = hawkes::HawkesProcess {
+        mu: 0.85,
+        alpha: 0.80,
+        beta: 1.0,
+    };
+
+    let cancel_orders_times = hawkes::HawkesProcess {
+        mu: 0.50,
+        alpha: 0.40,
+        beta: 1.1,
+    };
+
+    let limit_orders_times = hawkes::HawkesProcess {
+        mu: 0.90,
+        alpha: 0.81,
+        beta: 0.95,
+    };
+
+    let current_ts = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as f64;
+    
+    // -- Independent from each other but all start at the same time point (step by step)
+    
+    // -- Generate: Time of arrival, Side, Amount.
+    let _mo_times = market_orders_times.generate_values(current_ts, 2);
+    let _mo_sides: Vec<market::Side> = vec![];
+    let _mo_amounts: Vec<f64> = vec![]; 
+
+    // -- Generate: Time of arrival, Order Selection.
+    let _co_times = cancel_orders_times.generate_values(current_ts, 3);
+    let _co_orders: Vec<market::Order> = vec![];
+
+    // -- Generate: Time of arrival, Side, Amount, Price.
+    let _lo_times = limit_orders_times.generate_values(current_ts, 4);
+    let _lo_sides: Vec<market::Side> = vec![];
+    let _lo_amounts: Vec<f64> = vec![];
+    let _lo_price: Vec<f64> = vec![];
+
+    // Generate a Market Order {Buy|Sell}
+    
+    // Generate a New limit Order {Buy|Sell}
+    // a) Within the spread (from 0 to TOB)
+    // b) Outside the spread (Above Spread Below HOB)
+    // c) Outside the spread (Above HOB Below LOB)
+
+    // Generate a Cancelation of a Limit Order {Buy|Sell}
+    // a) Within the spread (from 0 to TOB)
+    // b) Outside the spread (Above Spread Below HOB)
+    // c) Outside the spread (Above HOB Below LOB)
+    
+}
