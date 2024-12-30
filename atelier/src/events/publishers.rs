003a4593f296480a2f63c36df6025f4e298ba8a1
IFFranciscoME <if.francisco.me@gmail.com>
2024-12-29 22:15:58 -0600

    Sync-up changes

--- atelier/src/events/publishers.rs
+++ atelier/src/events/publishers.rs
@@ -1,1 +1,34 @@
 // Publishers
+<<<<<<< HEAD
+=======
+
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::util::Timeout;
+use rdkafka::ClientConfig;
+use std::time::Duration;
+
+pub fn create() -> FutureProducer {
+    let mut config = ClientConfig::new();
+    config.set("bootstrap.servers", "localhost:9092");
+
+    let producer: FutureProducer = config.create().expect("Failure in message formation");
+
+    producer
+}
+
+pub async fn produce(future_producer: FutureProducer, msg: String) {
+    let k_topic = "test-topic";
+    let record = FutureRecord::to(k_topic)
+        .payload(msg.as_str())
+        .key("test-key");
+
+    let status_delivery = future_producer
+        .send(record, Timeout::After(Duration::from_secs(2)))
+        .await;
+
+    match status_delivery {
+        Ok(report) => println!("Event transmited {:?}", report),
+        Err(e) => println!("Event not transmited, produced this error {:?}", e),
+    }
+}
+>>>>>>> be5a583 (Progress on multi-commit mod)
